44b0f0bea0db1400361d108fc589f7749125c1a1
LikiPiki <sergey.batalev@yandex.ru>
2017-05-16 18:47:49 +0300

    update 1.0

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ keks = ['кек', 'кпек', 'КЕК', 'Кек', 'кееееееК', 'лол к
 ornul = ['как мразь', 'как тварь', 'как пидор', 'как шлеха', 'как алеша попович', 'как стерва']
 reply = ['Вот ты мышь...', 'Уйди лофтер!', 'Ты надоел уже', 'Шо, дизайнер шоли?', 'Обкекался мразь', 'Тоби пизда', 'Вали пидор', 'Соси писос', 'Член не дорос кукарекать']
 telki = ['Слишком много телок', 'Попробуй позже', 'Я отказываюсь выполнять твои команды']
+error = ['Телок дудосят', 'Хватит фапать бро', 'Что то пошло не так', 'Алеша попвич занят с телками, попробуй позже', 'Азиатки кончились']
+stikers = [
+	'CAADAwADDwMAAt0NBwl-2xGCWqBcTAI' # pesiy kek
+]
 
 @bot.message_handler(commands=['kek'])
 def handle_start_help(message):
@@ -47,16 +51,19 @@ def handle_start_help(message):
 			obj = json.loads(data)
 			counter += 1
 			for img in obj:
-				bot.send_message(message.chat.id, 'http://media.oboobs.ru/noise/' + str(img['id']) + '.jpg')
+				bot.send_photo(message.chat.id, 'http://media.oboobs.ru/noise/' + str(img['id']) + '.jpg')
 		except Exception:
-			bot.send_message(message.chat.id, 'Что то пошло не так...')
+			bot.send_message(message.chat.id, error[random.randint(0, 4)])
 	else:
 		bot.send_message(message.chat.id, telki[random.randint(0, 2)])
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, reply[random.randint(0, 8)])
+	if random.randint(0, 1) == 1:
+		bot.send_sticker(message.chat.id, stikers[random.randint(0, 0)])
+	else:
+	    bot.send_message(message.chat.id, reply[random.randint(0, 8)])
 
 bot.polling()
 
